56803d135c7bf8d1077f129d09681ca5396e371a
bouderton <rmkhazal@gmail.com>
2024-06-29 23:41:18 -0700

    footer markup

--- NewsExplorer/src/components/Footer/Footer.jsx
+++ NewsExplorer/src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import "./Footer.css";
+import github from "../../images/github.svg";
+import linkedin from "../../images/linkedin.svg";
 
 const Footer = () => {
   return (
@@ -7,9 +9,21 @@ const Footer = () => {
         <p className="footer__content-copyright">
           &copy; Supersite, Powered by News API
         </p>
-        <div className="footer__content-social">
-          <p>Home</p>
-          <p>TripleTen</p>
+        <div className="footer__content-list-container">
+          <ul className="footer__content-list">
+            <li className="footer__content_list-item">
+              <p className="footer__content_list-text">Home</p>
+            </li>
+            <li className="footer__content_list-item">
+              <p className="footer__content_list-text">TripleTen</p>
+            </li>
+            <li className="footer__content-list-item">
+              <img src={github} className="footer__content_list-img" />
+            </li>
+            <li className="footer__content-list-item">
+              <img src={linkedin} className="footer__content_list-img" />
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
