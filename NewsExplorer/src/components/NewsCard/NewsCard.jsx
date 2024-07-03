ec4babd653e4d00f420bec763220d5dbc034fe35
bouderton <rmkhazal@gmail.com>
2024-07-03 14:10:08 -0700

    news card grid setup

--- NewsExplorer/src/components/NewsCard/NewsCard.jsx
+++ NewsExplorer/src/components/NewsCard/NewsCard.jsx
@@ -1,18 +1,27 @@
 import "./NewsCard.css";
+import card_image_placeholder from "../../images/card-image-placeholder.png";
 
 const NewsCard = () => {
+  // cards accept news data
   return (
     <div className="card__container">
       <div className="card">
-        <img className="card__image" alt="News Card Image" />
+        <img
+          src={card_image_placeholder}
+          className="card__image"
+          alt="News Card Image"
+        />
         <p className="card__date">June 23rd 2020</p>
         <h3 className="card__title">Title</h3>
-        <p className="card__text">asdfkjl;asdlkfja;sldjfk</p>
+        <p className="card__text">
+          asdfkjl;asdlkfja;sldjfkasdfkjl; asdlkfja;sldjfkasdfkjl;asdlkfja
+          ;sldjfkasdfkjl;asdlkfja;sldjfkasdfkjl
+          ;asdlkfja;sldjfkasdfkjl;asdlkfja;sldjfk
+        </p>
         <p className="card__author">National Geographic</p>
       </div>
     </div>
   );
-  z;
 };
 
 export default NewsCard;
--- NewsExplorer/src/components/NewsCardList/NewsCardList.jsx
+++ NewsExplorer/src/components/NewsCardList/NewsCardList.jsx
@@ -2,9 +2,20 @@ import NewsCard from "../NewsCard/NewsCard";
 import "./NewsCardList.css";
 
 const NewsCardList = () => {
+  // need to pass news data to news cards
   return (
-    <div className="card__list-container">
-      <NewsCard />
+    <div className="card__results">
+      <h3 className="card__results-text">Search Results</h3>
+      <div className="card__grid-container">
+        <div className="card__grid">
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+        </div>
+      </div>
     </div>
   );
 };
