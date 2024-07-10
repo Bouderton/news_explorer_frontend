9f9a745a23dfde296083e16e287a6de13bbb22ad
bouderton <rmkhazal@gmail.com>
2024-07-10 10:25:49 -0700

    card section

--- NewsExplorer/src/components/NewsCardList/NewsCardList.jsx
+++ NewsExplorer/src/components/NewsCardList/NewsCardList.jsx
@@ -4,8 +4,8 @@ import "./NewsCardList.css";
 const NewsCardList = () => {
   // need to pass news data to news cards
   return (
-    <div className="card__results">
-      <h3 className="card__results-text">Search Results</h3>
+    <div className="card__section">
+      <h3 className="card__section-text">Search Results</h3>
       <div className="card__grid-container">
         <NewsCard />
         <NewsCard />
