cff065c2065db7723947c3659664a80c3b406f8f
bouderton <rmkhazal@gmail.com>
2024-07-10 12:40:38 -0700

    adaptive card grid

--- NewsExplorer/src/components/NewsCard/NewsCard.jsx
+++ NewsExplorer/src/components/NewsCard/NewsCard.jsx
@@ -1,23 +1,23 @@
 import "./NewsCard.css";
-import card_image_placeholder from "../../images/card-image-placeholder.png";
+import doggy from "../../images/doggy.jpg";
+import bookmark from "../../images/bookmark.svg";
 
 const NewsCard = () => {
   // cards accept news data
   return (
     <div className="card__container">
+      <button className="card__bookmark" type="button">
+        <img src={bookmark} className="card__bookmark-img" />
+      </button>
       <div className="card">
-        <img
-          src={card_image_placeholder}
-          className="card__image"
-          alt="News Card Image"
-        />
+        <img src={doggy} className="card__image" alt="News Card Image" />
         <div className="card__text-container">
-          <p className="card__date">June 23rd 2020</p>
-          <h3 className="card__title">Title</h3>
+          <p className="card__date">January 1st 2000</p>
+          <h3 className="card__title">Good Boy</h3>
           <p className="card__text">
-            asdfkjl;asdlkfja;sldjfkasdfkjl;
-            asdlkfja;sldjfkasdfkjl;asdlkfja;sldjfkasdfkjl;asdlkfja;sldjfkasdfkjl
-            ;asdlkfja;sldjfkasdfkjl;asdlkfja;sldjfk
+            whosagoodboywhosagoodboywhosagoodboywhosagoodboywhosagoodboy
+            whosagoodboywhosagoodboywhosagoodboy
+            whosagoodboywhosagoodboywhosagoodboywhosagoodboy
           </p>
           <p className="card__author">National Geographic</p>
         </div>
