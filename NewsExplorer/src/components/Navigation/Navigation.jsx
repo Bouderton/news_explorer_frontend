58d85b349d7d2c4405daf41f9aaffbbd271f98b4
bouderton <rmkhazal@gmail.com>
2024-07-23 10:38:15 -0700

    navigation mobile styling

--- NewsExplorer/src/components/Navigation/Navigation.jsx
+++ NewsExplorer/src/components/Navigation/Navigation.jsx
@@ -5,46 +5,51 @@ import NavigationMobile from "../NavigationMobile/NavigationMobile";
 
 const Navigation = ({ loggedIn, name, openPopup }) => {
   return (
-    <nav className="nav">
-      {loggedIn ? (
-        <>
-          <h2 className="nav__title-saved">NewsExplorer</h2>
-          <div className="nav__buttons-saved">
-            <button type="text" className="nav__button-saved">
-              <Link style={{ textDecoration: "none", color: "#1a1b22" }} to="/">
+    <>
+      <nav className="nav">
+        {loggedIn ? (
+          <>
+            <h2 className="nav__title-saved">NewsExplorer</h2>
+            <div className="nav__buttons-saved">
+              <button type="text" className="nav__button-saved">
+                <Link
+                  style={{ textDecoration: "none", color: "#1a1b22" }}
+                  to="/"
+                >
+                  Home
+                </Link>
+              </button>
+              <button type="text" className="nav__button-saved">
+                Saved Articles
+              </button>
+              <button type="text" className="nav__button-logout">
+                {name}
+                <img src={logout_icon} className="nav__button-logout-icon" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2 className="nav__title">NewsExplorer</h2>
+            <div className="nav__buttons">
+              <button type="text" className="nav__button">
                 Home
-              </Link>
-            </button>
-            <button type="text" className="nav__button-saved">
-              Saved Articles
-            </button>
-            <button type="text" className="nav__button-logout">
-              {name}
-              <img src={logout_icon} className="nav__button-logout-icon" />
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <h2 className="nav__title">NewsExplorer</h2>
-          <div className="nav__buttons">
-            <button type="text" className="nav__button">
-              Home
-            </button>
-            <button
-              type="text"
-              className="nav__button-register"
-              onClick={openPopup}
-            >
-              Sign In
-            </button>
-          </div>
-          <div className="nav__mobile-toggle">
-            <NavigationMobile />
-          </div>
-        </>
-      )}
-    </nav>
+              </button>
+              <button
+                type="text"
+                className="nav__button-register"
+                onClick={openPopup}
+              >
+                Sign In
+              </button>
+            </div>
+          </>
+        )}
+      </nav>
+      <div className="nav__mobile-toggle">
+        <NavigationMobile openPopup={openPopup} />
+      </div>
+    </>
   );
 };
 
--- NewsExplorer/src/components/NavigationMobile/NavigationMobile.jsx
+++ NewsExplorer/src/components/NavigationMobile/NavigationMobile.jsx
@@ -1,10 +1,28 @@
 import "./NavigationMobile.css";
 
-const NavigationMobile = () => {
+const NavigationMobile = ({ openPopup, isOpen }) => {
   return (
     <nav className="nav__mobile">
       <h2 className="nav__mobile-title">NewsExplorer</h2>
       <button type="button" className="nav__mobile-button" />
+      <div className={`nav__dropdown ${isOpen ? "nav__dropdown-opened" : ""}`}>
+        <div className="nav__dropdown-top">
+          <h2 className="nav__mobile-title">NewsExplorer</h2>
+          <button type="button" className="nav__dropdown-close-button" />
+        </div>
+        <div className="nav__mobile-buttons">
+          <button type="text" className="nav__home-button">
+            Home
+          </button>
+          <button
+            type="text"
+            className="nav__mobile-button-register"
+            onClick={openPopup}
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
     </nav>
   );
 };
