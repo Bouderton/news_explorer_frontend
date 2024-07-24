f904eb6487093c833bba23d85bce495df3923bce
bouderton <rmkhazal@gmail.com>
2024-07-24 11:42:50 -0700

    responsive nav bar

--- NewsExplorer/src/components/Navigation/Navigation.jsx
+++ NewsExplorer/src/components/Navigation/Navigation.jsx
@@ -6,48 +6,50 @@ import NavigationMobile from "../NavigationMobile/NavigationMobile";
 const Navigation = ({ loggedIn, name, openPopup, isSavedNews }) => {
   return (
     <>
-      <nav className="nav">
-        {loggedIn ? (
-          <>
-            <h2 className="nav__title-saved">NewsExplorer</h2>
-            <div className="nav__buttons-saved">
-              <button type="text" className="nav__button-saved">
-                <Link
-                  style={{ textDecoration: "none", color: "#1a1b22" }}
-                  to="/"
-                >
+      <div className="nav__container">
+        <nav className="nav">
+          {loggedIn ? (
+            <>
+              <h2 className="nav__title-saved">NewsExplorer</h2>
+              <div className="nav__buttons-saved">
+                <button type="text" className="nav__button-saved">
+                  <Link
+                    style={{ textDecoration: "none", color: "#1a1b22" }}
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </button>
+                <button type="text" className="nav__button-saved">
+                  Saved Articles
+                </button>
+                <button type="text" className="nav__button-logout">
+                  {name}
+                  <img src={logout_icon} className="nav__button-logout-icon" />
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h2 className="nav__title">NewsExplorer</h2>
+              <div className="nav__buttons">
+                <button type="text" className="nav__button">
                   Home
-                </Link>
-              </button>
-              <button type="text" className="nav__button-saved">
-                Saved Articles
-              </button>
-              <button type="text" className="nav__button-logout">
-                {name}
-                <img src={logout_icon} className="nav__button-logout-icon" />
-              </button>
-            </div>
-          </>
-        ) : (
-          <>
-            <h2 className="nav__title">NewsExplorer</h2>
-            <div className="nav__buttons">
-              <button type="text" className="nav__button">
-                Home
-              </button>
-              <button
-                type="text"
-                className="nav__button-register"
-                onClick={openPopup}
-              >
-                Sign In
-              </button>
-            </div>
-          </>
-        )}
-      </nav>
-      <div className="nav__mobile-toggle">
-        <NavigationMobile openPopup={openPopup} isSavedNews={isSavedNews} />
+                </button>
+                <button
+                  type="text"
+                  className="nav__button-register"
+                  onClick={openPopup}
+                >
+                  Sign In
+                </button>
+              </div>
+            </>
+          )}
+        </nav>
+        <div className="nav__mobile-toggle">
+          <NavigationMobile openPopup={openPopup} isSavedNews={isSavedNews} />
+        </div>
       </div>
     </>
   );
