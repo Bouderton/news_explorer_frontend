5a42068b88f15e5fe39a71c081e85c361f22f5e5
bouderton <rmkhazal@gmail.com>
2024-07-31 10:50:53 -0700

    not found component (may be seperated later)

--- NewsExplorer/src/components/Preloader/Preloader.jsx
+++ NewsExplorer/src/components/Preloader/Preloader.jsx
@@ -1,7 +1,33 @@
 import "./Preloader.css";
+import not_found from "../../images/not_found.svg";
 
-const Preloader = () => {
-  return <div className="circle__preloader"></div>;
+const Preloader = ({ searching }) => {
+  return (
+    <div className="preloader__container">
+      {searching ? (
+        <>
+          <div className="circle-preloader"></div>
+          <p className="preloader__text">Searching for news...</p>
+        </>
+      ) : (
+        <>
+          <div className="notfound__container">
+            <img
+              src={not_found}
+              className="notfound__img"
+              alt="Not Found Icon"
+            />
+            <div className="notfound__text-container">
+              <h2 className="notfound__header">Nothing Found</h2>
+              <p className="notfound__text">
+                Sorry, but nothing matched your search terms
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Preloader;
