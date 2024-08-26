e846bce799ed9890cec0ace8a7e6dbbfeb2ad136
bouderton <rmkhazal@gmail.com>
2024-08-25 23:37:22 -0700

    NavigationSaved component

--- NewsExplorer/src/components/Navigation/Navigation.jsx
+++ NewsExplorer/src/components/Navigation/Navigation.jsx
@@ -2,9 +2,10 @@ import "./Navigation.css";
 import { Link, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import logout_light from "../../images/logout_light.svg";
-import logout_dark from "../../images/logout_dark.svg";
+
 import { UserContext } from "../../contexts/UserContext";
 import NavigationMobile from "../NavigationMobile/NavigationMobile";
+import NavigationSaved from "../NavigationSaved/NavigationSaved";
 
 const Navigation = ({ loggedIn, openPopup, isSavedNews, handleLogout }) => {
   const route = useLocation();
@@ -71,50 +72,7 @@ const Navigation = ({ loggedIn, openPopup, isSavedNews, handleLogout }) => {
           </nav>
         ) : (
           // SAVED NEWS NAV BAR (DARK MODE)
-          <>
-            <nav
-              className="nav__content"
-              style={{ borderBottom: "0.5px solid", borderColor: "#1a1b22" }}
-            >
-              <h2 className="nav__title" style={{ color: "#1a1b22" }}>
-                NewsExplorer
-              </h2>
-              <div className="nav__buttons" style={{ gap: "15px" }}>
-                <button type="text" className="nav__button">
-                  <Link
-                    to="/"
-                    style={{ textDecoration: "none", color: "#1a1b22" }}
-                  >
-                    Home
-                  </Link>
-                </button>
-                <hr className="nav__outline-black" />
-                <button
-                  type="text"
-                  className="nav__button-saved"
-                  style={{ color: "#1a1b22" }}
-                >
-                  Saved Articles
-                </button>
-                <Link
-                  to="/"
-                  style={{ textDecoration: "none", color: "#1a1b22" }}
-                >
-                  <button
-                    type="text"
-                    className="nav__button-logout dark"
-                    onClick={handleLogout}
-                  >
-                    {currentUser?.data.name}
-                    <img
-                      src={logout_dark}
-                      className="nav__button-logout-icon"
-                    />
-                  </button>
-                </Link>
-              </div>
-            </nav>
-          </>
+          <NavigationSaved handleLogout={handleLogout} />
         )}
         {/* Nav Bar Mobile Mode */}
         <div className="nav__mobile-toggle">
